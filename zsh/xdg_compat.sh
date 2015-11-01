385650c19919c9e0fe72418bb2256f010ea71eb3
Tomasz Kramkowski <tk@the-tk.com>
2015-11-01 13:22:53 +0000

    zsh: xdg_compat.sh: msmtp alias

--- zsh/xdg_compat.sh
+++ zsh/xdg_compat.sh
@@ -1,4 +1,5 @@
 alias elinks="elinks -config-dir $XDG_CONFIG_HOME/elinks"
 alias weechat="weechat --dir $XDG_CONFIG_HOME/weechat"
 alias mutt="mutt -F $XDG_CONFIG_HOME/mutt/config"
+alias msmtp="msmtp -C $XDG_CONFIG_HOME/msmtp/config"
 export VIMINIT='source $XDG_CONFIG_HOME/vim/vimrc'
