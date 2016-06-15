f5f85eeed4edc0782a46dc339ae0e6fe32eca332
Tomasz Kramkowski <tk@the-tk.com>
2016-06-15 20:06:22 +0100

    offlineimap

--- offlineimap/pythonfile.py
+++ offlineimap/pythonfile.py
@@ -2,4 +2,4 @@
 from subprocess import check_output
 
 def password(account, domain):
-    return check_output("pass mail/" + account + "@" + domain, shell=True).rstrip()
+    return check_output("pass mail/" + account + "@" + domain, shell=True).rstrip().split('\n')[0]
